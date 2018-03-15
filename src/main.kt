2d75a41294f83e629dc08818b2f1c74db34ec37b
Joe <jsarabia1247@gmail.com>
2018-03-15 12:48:49 -0400

    Delete main.kt

--- src/main.kt
+++
@@ -1,7 +0,0 @@
-import org.apache.commons.codec.digest.DigestUtils
-
-fun main(args : Array<String>) {
-    var hash = DigestUtils.sha1Hex("Joe")
-    var svg = toSvg(hash, 512, null)
-    println(svg)
-}
